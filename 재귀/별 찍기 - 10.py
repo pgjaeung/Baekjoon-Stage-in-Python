d46873f75343c70dbab3ac6c3a325fc4bf130fe0
pgjaeung <64916999+pgjaeung@users.noreply.github.com>
2023-04-05 23:17:25 +0900

    Add files via upload

---
+++ 재귀/별 찍기 - 10.py
@@ -0,0 +1,19 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+def star(len):
+    if len == 1:
+        return ['*']
+    stars = star(len//3)
+    l = []
+
+    for first in stars:
+        l.append(first*3)
+    for second in stars:
+        l.append(second+' '*(len//3)+second)
+    for third in stars:
+        l.append(third*3)
+    return l
+
+n = int(input())
+print('\n'.join(star(n)))
