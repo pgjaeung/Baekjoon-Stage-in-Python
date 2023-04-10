072e85a4800d06a11c4728cafe2acd32a3dcaece
pgjaeung <64916999+pgjaeung@users.noreply.github.com>
2023-04-11 00:09:56 +0900

    Add files via upload
    if를 and로 바꿈

--- 리스트/하얀 칸.py
+++ 리스트/하얀 칸.py
@@ -1,11 +1,9 @@
 n = []
 for _ in range(8):
     n.append(list(map(str,list(input()))))
-
 a=0
 for i in range(8):
     for j in range(8):
-        if (i+j) % 2 == 0:
-            if n[i][j] == 'F':
-                a = a+1
+        if (i+j) % 2 == 0 and n[i][j] == 'F':
+            a = a + 1
 print(a)
